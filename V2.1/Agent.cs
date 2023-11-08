256285aef65caecdc7dcb65516a6005dd9aad3a2
MaxWesterlund <95910612+MaxWesterlund@users.noreply.github.com>
2023-11-08 12:13:42 +0100

    .

--- V2.1/Agent.cs
+++ V2.1/Agent.cs
@@ -16,7 +16,7 @@ public class Agent {
 	}
 	
 	public Action State;
-	Random rng;
+	readonly Random rng;
 
 	public Agent(int xStart, int yStart) {
 		X = xStart;
@@ -29,72 +29,84 @@ public class Agent {
 		Heading = (float)rng.NextDouble() * 2 * MathF.PI;
 	}
 
+	private struct Sensor {
+		public float Value;
+		public int ID;
+	}
+
 	public void Sense(Scene scene) {
-		// float[] sensors = new float[3];
-		int sensor = 0;
-		float maxVal = 0;
+		Sensor[] sensors = new Sensor[3];
+		int height = scene.Grid[X, Y].Height;
+
 		for (int i = 0; i < 3; i++) {
-			float a = Heading + (i - 1) * Settings.AgentSensorAngle;
-			int x = (int)MathF.Round(realX + MathF.Cos(a) * Settings.AgentSensorDistance);
-			int y = (int)MathF.Round(realY + MathF.Sin(a) * Settings.AgentSensorDistance);
-			// if (!scene.IsOutOfBounds(x, y)) {
-			// 	sensors[i] = scene.Grid[X, y].Evaluate(0);
-			// }
-			// else {
-			// 	sensors[i] = float.MinValue;
-			// }
-			float val = scene.IsOutOfBounds(x, y) ?  0 : scene.Grid[X, y].Evaluate(0);
-			if (i == 0) {
-				maxVal = val;
-				sensor = 2;
+			sensors[i].ID = i;
+
+			float angle = Heading + (i - 1) * Settings.AgentSensorAngle;
+			int x = (int)MathF.Round(realX + MathF.Cos(angle) * Settings.AgentSensorDistance);
+			int y = (int)MathF.Round(realY + MathF.Sin(angle) * Settings.AgentSensorDistance);
+
+			if (scene.IsOutOfBounds(x, y)) {
+				sensors[i].Value = float.MinValue;
 				continue;
 			}
-			if (val > maxVal) {
-				maxVal = val;
-				sensor = i + 2;
-			}
-			else if (val == maxVal) {
-				sensor += i + 2;
-			}
+			sensors[i].Value = scene.Grid[x, y].Evaluate(height);
 		}
 
-		switch (sensor) {
-			case 2: // Left
-				Heading -= Settings.AgentSensorAngle;
-				break;
-			case 4: // Right
-				Heading += Settings.AgentSensorAngle;
-				break;
-			case 5: // Left & Middle
-				Heading -= rng.Next(2) == 1 ? 0 : Settings.AgentSensorAngle;
-				break;
-			case 6: // Left & Right
-				Heading += rng.Next(2) == 1 ? Settings.AgentSensorAngle : -Settings.AgentSensorAngle;
-				break;
-			case 7: // Middle & Right
-				Heading += rng.Next(2) == 1 ? 0 : Settings.AgentSensorAngle;
-				break;
-			case 8: // Left & Middle & Right
-				Heading += Settings.AgentSensorAngle * (rng.Next(3) -1);
-				break;
+		Array.Sort(sensors, (a, b) => b.Value.CompareTo(a.Value));
+	
+		if (sensors[0].Value == float.MinValue) {
+			// random dir
+			Heading += (rng.Next(3) - 1) * Settings.AgentSensorAngle;
 		}
-		
-		// float max = MathF.Max(MathF.Max(sensors[0], sensors[2]), sensors[1]);
+		else if (sensors[0].Value == sensors[1].Value) {
+			// chose random of top 2
+			Heading += (sensors[rng.Next(1)].ID - 1) * Settings.AgentSensorAngle;
+		}
+		else {
+			Heading += (sensors[0].ID - 1) * Settings.AgentSensorAngle;
+		}
 
-		// if (sensors[2] == sensors[0] && sensors[0] == sensors[1]) {
-		// 	Heading += Settings.AgentSensorAngle * (rng.Next(3) -1);
-		// }
-		// else if (sensors[0] == sensors[2]) {
-		// 	Heading += rng.Next(2) == 1 ? Settings.AgentSensorAngle : -Settings.AgentSensorAngle;
-		// }
-		// else if (max == sensors[0]) {
-		// 	Heading -= Settings.AgentSensorAngle;
-		// }
-		// else if (max == sensors[2]) {
-		// 	Heading += Settings.AgentSensorAngle;
+		// int sensor = 0;
+		// float maxVal = 0;
+		// for (int i = 0; i < 3; i++) {
+		// 	float a = Heading + (i - 1) * Settings.AgentSensorAngle;
+		// 	int x = (int)(realX + MathF.Cos(a) * Settings.AgentSensorDistance);
+		// 	int y = (int)(realY + MathF.Sin(a) * Settings.AgentSensorDistance);
+		// 	float val = scene.IsOutOfBounds(x, y) ?  0 : scene.Grid[X, y].Evaluate(0);
+		// 	if (i == 0) {
+		// 		maxVal = val;
+		// 		sensor = 2;
+		// 		continue;
+		// 	}
+		// 	if (val > maxVal) {
+		// 		maxVal = val;
+		// 		sensor = i + 2;
+		// 	}
+		// 	else if (val == maxVal) {
+		// 		sensor += i + 2;
+		// 	}
 		// }
 
-		// return;
+		// switch (sensor) {
+		// 	case 2: // Left
+		// 		Heading -= Settings.AgentSensorAngle;
+		// 		break;
+		// 	case 4: // Right
+		// 		Heading += Settings.AgentSensorAngle;
+		// 		break;
+		// 	case 5: // Left & Middle
+		// 		Heading -= rng.Next(2) == 1 ? 0 : Settings.AgentSensorAngle;
+		// 		break;
+		// 	case 6: // Left & Right
+		// 		Heading += rng.Next(2) == 1 ? Settings.AgentSensorAngle : -Settings.AgentSensorAngle;
+		// 		break;
+		// 	case 7: // Middle & Right
+		// 		Heading += rng.Next(2) == 1 ? 0 : Settings.AgentSensorAngle;
+		// 		break;
+		// 	case 8: // Left, Middle & Right
+		// 		Heading += Settings.AgentSensorAngle * (rng.Next(3) -1);
+		// 		break;
+		// }
 	}
 	
 	public void Move(ref Scene scene) {
